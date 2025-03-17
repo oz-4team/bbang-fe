3d71ce211967f6f2430dc0ad7f5c4f549b82119d
dwqcwda <lty5582@naver.com>
2025-03-17 16:04:06 +0900

    ⚡️ App.jsx router
    app.jsx에 연결

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,27 @@ import MainLayout from "./layouts/MainLayout";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import SignUpPage from "./pages/SignUpPage";
-import ForgotPasswordPage from "./pages/ForgotPasswordPage";
-import ResetPassword from "./pages/ResetPasswordPage";
+import ForgotPasswordPage from "./pages/ForgotPasswordPage"; // ✅ 추가됨
+import ResetPasswordPage from "./pages/ResetPasswordPage"; // ✅ 수정됨
 
 function App() {
-
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/forgot-password" element={<ForgotPasswordPage />} />
-        <Route path="/reset-password" element={<ResetPassword />} />  {/* ✅ 추가 */}
-      </Routes>
-    </Router>
+    <>
+      <Router>
+        <Routes>
+          <Route element={<MainLayout />}>
+            <Route index path="/" element={<HomePage />} />
+          </Route>
+
+          <Route element={<EmptyLayout />}>
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/signup" element={<SignUpPage />} />
+            <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+            <Route path="/reset-password" element={<ResetPasswordPage />} /> 
+          </Route>
+        </Routes>
+      </Router>
+    </>
   );
 }
 
