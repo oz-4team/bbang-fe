9a81ba4638375b055045a7ddadbefdb4b1ce905a
Hyunjung0221 <ekfqlc4472@naver.com>
2025-03-18 11:56:59 +0900

    ✨ 관리자 권한 신청 완료 페이지
    관리자 권한 신청 후 관리자 권한 신청 완료 페이지 -> 홈페이지

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import SignupCompletePage from "./pages/SignupCompletedPage"; // 추가함
 import ForgotPasswordPage from "./pages/ForgotPasswordPage"; // ✅ 추가됨
 import ResetPasswordPage from "./pages/ResetPasswordPage"; // ✅ 수정됨
 import AdminRequestPage from "./pages/AdminRequestPage";
+import AdminRequestCompletedPage from "./pages/AdminRequestCompletedPage";
 
 function App() {
   return (
@@ -26,6 +27,7 @@ function App() {
             <Route path="/reset-password" element={<ResetPasswordPage />} />
             <Route path="/signup-complete" element={<SignupCompletePage />} />
             <Route path="/adminrequest" element={<AdminRequestPage />} />
+            <Route path="/admin-request-completed" element={<AdminRequestCompletedPage />} />
           </Route>
         </Routes>
       </Router>
