f183d74156deea40fd4fb8d6939184478dfd013e
dwqcwda <lty5582@naver.com>
2025-03-18 15:54:55 +0900

    🐛 Login sigin up
    로그인 회원가입 api수정

--- src/components/SocialLogin.jsx
+++ src/components/SocialLogin.jsx
@@ -1,15 +1,21 @@
+import React from "react";
 import { GOOGLE_AUTH_URL, KAKAO_AUTH_URL, NAVER_AUTH_URL } from "../api/socialAuthApi";
+import googleLogo from '../assets/images/googleLogo.png';
+import naverLogo from '../assets/images/naverLogo.png';
+import kakaoLogo from '../assets/images/kakaoLogo.png';
 
 function SocialLogin() {
-  const handleSocialLogin = (authUrl) => {
-    window.location.href = authUrl;
-  };
-
   return (
-    <div className="social-buttons">
-      <button onClick={() => handleSocialLogin(NAVER_AUTH_URL)}>네이버 로그인</button>
-      <button onClick={() => handleSocialLogin(KAKAO_AUTH_URL)}>카카오 로그인</button>
-      <button onClick={() => handleSocialLogin(GOOGLE_AUTH_URL)}>구글 로그인</button>
+    <div className="social-login">
+      <button onClick={() => (window.location.href = NAVER_AUTH_URL)} className="social-button">
+        <img src={naverLogo} alt="Naver Logo" className="social-icon" />
+      </button>
+      <button onClick={() => (window.location.href = KAKAO_AUTH_URL)} className="social-button">
+        <img src={kakaoLogo} alt="Kakao Logo" className="social-icon" />
+      </button>
+      <button onClick={() => (window.location.href = GOOGLE_AUTH_URL)} className="social-button">
+        <img src={googleLogo} alt="Google Logo" className="social-icon" />
+      </button>
     </div>
   );
 }
