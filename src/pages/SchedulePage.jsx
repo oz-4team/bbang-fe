b7e2fba2d3accd2bf85b0617c6a72338a774e7c2
sasha <sasha.park.designer@gmail.com>
2025-03-19 10:52:20 +0900

    ⚡️wip: 중간저장

--- src/pages/SchedulePage.jsx
+++ src/pages/SchedulePage.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import MyArtistFilterCard from "../components/MyArtistFilterCard";
 
 const SchedulePage = () => {
   const navigateToDetails = () => {
@@ -6,9 +7,63 @@ const SchedulePage = () => {
   };
 
   return (
-    <div>
-      {/* <div>schedule</div> */}
-      <button onClick={navigateToDetails}>디테일페이지로 이동</button>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        width: "100vw",
+        padding: "2rem 1rem",
+      }}
+    >
+      <div
+        style={{
+          width: "100%",
+          maxWidth: "800px",
+          backgroundColor: "lightgray",
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            gap: "2rem",
+            alignItems: "flex-start",
+            paddingBottom: "2rem",
+          }}
+        >
+          <div style={{ fontSize: "24px", fontWeight: "bold" }}>
+            마이아티스트
+          </div>
+        </div>
+        <div
+          style={{
+            display: "-webkit-inline-box",
+            gap: "1rem",
+            // overflowX: "scroll",
+            overflow: "scroll",
+          }}
+        >
+          <MyArtistFilterCard />
+          <MyArtistFilterCard />
+          <MyArtistFilterCard />
+          <MyArtistFilterCard />
+          <MyArtistFilterCard />
+          <MyArtistFilterCard />
+          <MyArtistFilterCard />
+          <MyArtistFilterCard />
+          <MyArtistFilterCard />
+          <MyArtistFilterCard />
+          <MyArtistFilterCard />
+          <MyArtistFilterCard />
+          <MyArtistFilterCard />
+          <MyArtistFilterCard />
+          <MyArtistFilterCard />
+          <MyArtistFilterCard />
+        </div>
+
+        <button onClick={navigateToDetails}>디테일페이지로 이동</button>
+      </div>
     </div>
   );
 };
