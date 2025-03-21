306636a0c9edb43cf57b6d2df5a4620112828dd9
sasha <sasha.park.designer@gmail.com>
2025-03-21 21:24:28 +0900

    wip

--- src/components/MyArtistFilter.jsx
+++ src/components/MyArtistFilter.jsx
@@ -1,15 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
+import { TiArrowSortedDown } from "react-icons/ti";
+import Modal from "./Modal";
 
 const MyArtistFilter = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const handleclickMyArtistFilter = (e) => {
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
+  const handlePrimaryClick = () => {
+    console.log("Primary button clicked");
+    handleCloseModal();
+  };
+
+  const handleSecondaryClick = () => {
+    console.log("Secondary button clicked");
+    handleCloseModal();
+  };
+
   return (
-    <div style={{ minWidth: "300px", marginLeft: "-10px" }}>
-      <select>
-        <option value="all">마이 아티스트</option>
-        <option value="artist1">Artist 1</option>
-        <option value="artist2">Artist 2</option>
-        <option value="artist3">Artist 3</option>
-      </select>
-    </div>
+    <>
+      <div>
+        <button
+          style={{
+            minWidth: "300px",
+            width: "100%",
+            height: "60px",
+            textAlign: "left",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between",
+          }}
+          onClick={handleclickMyArtistFilter}
+        >
+          마이 아티스트
+          <span style={{ marginLeft: "auto" }}>
+            <TiArrowSortedDown />
+          </span>
+        </button>
+      </div>
+      {isModalOpen && (
+        <Modal
+          title="로그인이 필요해요 ☺️"
+          description="로그인하시고 최애 정보를 확인해보세요!"
+          primaryButtonText="로그인하러 가기"
+          secondaryButtonText="닫기"
+          onPrimaryClick={handlePrimaryClick}
+          onSecondaryClick={handleSecondaryClick}
+        />
+      )}
+    </>
   );
 };
 
