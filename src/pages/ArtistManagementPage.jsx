ea68ed763ccb8751f856bcd704ab8ff6e9028ecb
sasha <sasha.park.designer@gmail.com>
2025-03-27 14:50:18 +0900

    🐛 Fix artistGroups handling and update rendering logic in ArtistManagementPage

--- src/pages/ArtistManagementPage.jsx
+++ src/pages/ArtistManagementPage.jsx
@@ -5,13 +5,36 @@ import ArtistInfoSolo from "../components/add-artist/ArtistInfoSolo";
 import SelectArtistType from "../components/add-artist/SelectArtistType";
 
 const ArtistManagementPage = () => {
-  const { artistGroups } = useReadArtistGroups();
+  const { artistGroups = { artists: [], artist_groups: [] }, loading } =
+    useReadArtistGroups();
   console.log("artistGroups:", artistGroups);
+
+  // const [addedArtistType, setAddedArtistType] = React.useState(null);
+  // const handleAddedArtistTypeChange = (type) => {
+  //   setAddedArtistType(type);
+  //   console.log("추가 아티스트 타입 변경:", addedArtistType);
+  // };
+
+  // const addedArtistType =
+  //   artistGroups?.artists.length > 0 && artistGroups?.artist_groups.length === 0
+  //     ? "solo"
+  //     : artistGroups?.artist_groups.length > 0 &&
+  //       artistGroups?.artists.length === 0
+  //     ? "group"
+  //     : artistGroups?.artists.length > 0 &&
+  //       artistGroups?.artist_groups.length > 1
+  //     ? "multiple"
+  //     : artistGroups?.artists.length === 0 &&
+  //       artistGroups?.artist_groups.length > 0
+  //     ? "group-adding"
+  //     : null;
+
   const [artistType, setArtistType] = React.useState(null);
   const handleArtistTypeChange = (type) => {
     setArtistType(type);
     console.log("아티스트 타입 변경aaaa:", artistType);
   };
+
   useEffect(() => {
     console.log("아티스트 타입 변경:", artistType);
   }, [artistType]);
@@ -19,13 +42,23 @@ const ArtistManagementPage = () => {
   return (
     <>
       <div className="outlet-container">
-        <div className="inner p-24">
-          <SelectArtistType setArtistType={handleArtistTypeChange} />
-          {artistType === "group" && (
-            <ArtistInfoGroup artistType={artistType} />
-          )}
-          {artistType === "solo" && <ArtistInfoSolo artistType={artistType} />}
-        </div>
+        {artistGroups?.length === 0 ? (
+          <div className="inner p-24">
+            <SelectArtistType setArtistType={handleArtistTypeChange} />
+            {artistType === "group" && (
+              <ArtistInfoGroup artistType={artistType} />
+            )}
+            {artistType === "solo" && (
+              <ArtistInfoSolo artistType={artistType} />
+            )}
+          </div>
+        ) : (
+          <div>
+            {/* <p>{addedArtistType}</p> */}
+            <p>Artist Groups Count: {artistGroups?.artists.length}</p>
+            <p>Artist Groups Count: {artistGroups?.artist_groups.length}</p>
+          </div>
+        )}
       </div>
     </>
   );
