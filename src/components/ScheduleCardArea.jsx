2a28f28f8759fe771bd4bade0b856a1463dff4c8
sasha <sasha.park.designer@gmail.com>
2025-03-30 12:48:57 +0900

    ✨ 기본 이미지 추가 및 스케줄 카드 이미지 로직 개선

--- src/components/ScheduleCardArea.jsx
+++ src/components/ScheduleCardArea.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { fetchAllSchedules } from "../api/schedule/scheduleApi";
+import defaultImage from "../assets/images/img-defualt.png";
 import ScheduleCard from "./ScheduleCard";
 
 const ScheduleCardArea = ({ onCardClick }) => {
@@ -29,28 +30,24 @@ const ScheduleCardArea = ({ onCardClick }) => {
       style={{
         display: "flex",
         flexDirection: "column",
-        // gridTemplateColumns: "repeat(auto-fill, minmax(150px, 1fr))",
         gap: "16px",
       }}
     >
       {schedules.map((a) => (
-        <>
-          <ScheduleCard
-            key={a.id}
-            name={a.artist ? a.artist.artist_name : a.artist_group.artist_group}
-            image={
-              a.image_url
-                ? a.image_url
-                : a.artist
-                ? a.artist.image_url
-                : a.artist_group.image_url
-            }
-            title={a.title}
-            id={a.id}
-            onCardClick={() => onCardClick()}
-            is_favorited={a.is_favorited}
-          />
-        </>
+        <ScheduleCard
+          key={a.id}
+          name={a.artist ? a.artist.artist_name : a.artist_group.artist_group}
+          image={
+            a.image_url ||
+            (a.artist && a.artist.image_url) ||
+            (a.artist_group && a.artist_group.image_url) ||
+            defaultImage
+          }
+          title={a.title}
+          id={a.id}
+          onCardClick={() => onCardClick()}
+          is_favorited={a.is_favorited}
+        />
       ))}
     </div>
   );
