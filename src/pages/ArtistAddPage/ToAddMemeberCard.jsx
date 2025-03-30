bac0c0ce13ff48b2a0ca254069d5187f8d999781
sasha <sasha.park.designer@gmail.com>
2025-03-30 14:33:55 +0900

    ✨ Added member card component for adding new members to artist groups and removed the old added member component

---
+++ src/pages/ArtistAddPage/ToAddMemeberCard.jsx
@@ -0,0 +1,93 @@
+import React, { useEffect, useState } from "react";
+import useArtistGroups from "../../api/artist/useArtistGroups";
+
+const ToAddMemeberCard = ({ group }) => {
+  console.log("group:", group);
+
+  const [groupId, setgroupId] = useState(group.id);
+
+  useEffect(() => {
+    setgroupId(group.id);
+  }, [group.id]);
+
+  const { createArtistForGroup } = useArtistGroups();
+
+  const [toAddMember, setToAddMember] = useState({
+    artist_name: "",
+    artist_agency: group.artist_agency,
+    group_insta: group.group_insta,
+    artist_group_fandom: group.group_fandom,
+    debut_date: group.debut_date,
+    solomembers: "",
+    image_url: null,
+    artist_insta: "",
+  });
+
+  const handleImageUpload = (event, callback) => {
+    const file = event.target.files[0];
+
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (e) => callback(e.target.result);
+      reader.readAsDataURL(file);
+    }
+  };
+
+  const addMember = (groupId) => {
+    console.log("groupId:", groupId);
+    createArtistForGroup(groupId, [toAddMember]);
+  };
+
+  return (
+    <div className="member-container">
+      <div className="member-card">
+        <div className="member-photo">
+          {toAddMember.image_url && (
+            <img src={toAddMember.image_url} alt="멤버 사진" />
+          )}
+          <label className="upload-button">
+            업로드
+            <input
+              type="file"
+              accept="image/*"
+              onChange={(e) =>
+                handleImageUpload(e, (img) =>
+                  setToAddMember({ ...toAddMember, image_url: img })
+                )
+              }
+              hidden
+            />
+          </label>
+        </div>
+
+        <div>{JSON.stringify(toAddMember)}</div>
+        <input
+          type="text"
+          placeholder="멤버 이름"
+          value={toAddMember.artist_name}
+          onChange={(e) => {
+            setToAddMember({ ...toAddMember, artist_name: e.target.value });
+          }}
+        />
+
+        <input
+          type="text"
+          placeholder={`멤버${+1} 인스타그램 링크`}
+          value={toAddMember.artist_insta}
+          onChange={(e) => {
+            setToAddMember({ ...toAddMember, artist_insta: e.target.value });
+          }}
+        />
+
+        <button
+          className="btn-primary bg-none"
+          onClick={() => addMember(groupId, [toAddMember])}
+        >
+          추가하기
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default ToAddMemeberCard;
