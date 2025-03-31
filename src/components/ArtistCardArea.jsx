55168bee5600f832108dae1525d0a96ed5d05038
sasha <sasha.park.designer@gmail.com>
2025-03-31 17:33:08 +0900

    ✨ Feat: ArtistCardArea 컴포넌트에서 불필요한 코드 주석 처리 및 아티스트 데이터 표시 방식 개선

--- src/components/ArtistCardArea.jsx
+++ src/components/ArtistCardArea.jsx
@@ -7,30 +7,32 @@ import ArtistCard from "./ArtistCard";
 const ArtistCardArea = ({ onCardClick, searchQuery }) => {
   const { artistAndGroups } = useReadArtistAndGroups();
 
+  console.log("artistAndGroups:", artistAndGroups);
+
   if (!artistAndGroups || artistAndGroups.length === 0) {
     return <div>loading...</div>;
   }
 
-  const shuffledArtists = [...artistAndGroups.data].sort(
-    () => Math.random() - 0.5
-  );
+  // const shuffledArtists = [...artistAndGroups.data].sort(
+  //   () => Math.random() - 0.5
+  // );
 
-  // 그룹에 속해있는 아티스트는 제외
-  let filteredArtists = shuffledArtists.filter(
-    (a) => !(a.artist_name && a.artist_group)
-  );
+  // // 그룹에 속해있는 아티스트는 제외
+  // let filteredArtists = shuffledArtists.filter(
+  //   (a) => !(a.artist_name && a.artist_group)
+  // );
 
   // 검색어로 필터링
   if (searchQuery) {
     const q = searchQuery.toLowerCase();
-    filteredArtists = filteredArtists.filter((a) =>
+    artistAndGroups.data = artistAndGroups.data.filter((a) =>
       (a.artist_name || a.artist_group).toLowerCase().includes(q)
     );
   }
 
   return (
     <div className="artist-card-area">
-      {filteredArtists.length > 0 ? (
+      {/* {filteredArtists.length > 0 ? (
         filteredArtists.map((a, index) => (
           <ArtistCard
             key={index}
@@ -44,7 +46,19 @@ const ArtistCardArea = ({ onCardClick, searchQuery }) => {
         ))
       ) : (
         <div className="no-results">검색 결과가 없습니다.</div>
-      )}
+      )} */}
+
+      {artistAndGroups.data.map((a, index) => (
+        <ArtistCard
+          key={index}
+          name={a.artist_name || a.artist_group}
+          image={a.image_url || defaultImage}
+          type={a.artist_name ? "solo" : "group"}
+          id={a.id}
+          onCardClick={() => onCardClick()}
+          is_liked={a.is_liked}
+        />
+      ))}
     </div>
   );
 };
