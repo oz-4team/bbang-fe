564e472de9a1daecb984bf48e309ab2434f29668
sasha <sasha.park.designer@gmail.com>
2025-04-01 23:07:31 +0900

    ✨ Feat: 인증 URL의 리다이렉트 URI를 배포 환경으로 변경

--- src/api/kakaoAuthapi.js
+++ src/api/kakaoAuthapi.js
@@ -1,13 +1,13 @@
 import axios from "axios";
-import { saveToken, getToken } from "../utils/authUtils";
 import useUserStore from "../store/userStore";
+import { saveToken } from "../utils/authUtils";
 
 // 백엔드 주소 및 환경 변수 설정
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://3.35.108.208:8000";
 const USE_BACKEND = true; // 백엔드 활성화 여부
 
 // Kakao 인증 URL (리다이렉트 URI에는 provider 정보 없이 설정)
-export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${import.meta.env.VITE_KAKAO_CLIENT_ID}&redirect_uri=http://localhost:5173/auth/kakao/callback/&response_type=code`;
+export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${import.meta.env.VITE_KAKAO_CLIENT_ID}&redirect_uri=https://idolsync.kro.kr/auth/kakao/callback/&response_type=code`;
 
 // Kakao 로그인 함수
 export const exchangeKakaoToken = async (authCode, navigate) => {
@@ -18,7 +18,7 @@ export const exchangeKakaoToken = async (authCode, navigate) => {
 
     if (USE_BACKEND) {
         try {
-          
+
             // 백엔드에 인가 코드 전달 (POST 요청)
             const response = await axios.post(`${API_BASE_URL}/auth/kakao/callback/`, {
                 code: authCode,
