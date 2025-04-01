e0e6a80e500aabcbe32d6dbbbec501bea69ff9e4
sasha <sasha.park.designer@gmail.com>
2025-04-01 15:37:32 +0900

    ✨ Feat: 초기 로딩 상태를 true로 설정하고 디버깅 로그 추가

--- src/api/artist/useLikes.js
+++ src/api/artist/useLikes.js
@@ -4,7 +4,7 @@ const API_BASE_URL = import.meta.env.VITE_API_BASE_URL
 
 const useLikes = () => {
     const accessToken = localStorage.getItem("authToken");
-    const [loading, setLoading] = useState(false);
+    const [loading, setLoading] = useState(true);
     const [like, setLike] = useState([]);
     const [likes, setLikes] = useState([]);
 
@@ -27,7 +27,7 @@ const useLikes = () => {
         }
         catch (error) {
             console.error("Error reading readLike:", error);
-
+            // console.log("accessToken:", accessToken);
 
         } finally {
             setLoading(false);
@@ -50,7 +50,9 @@ const useLikes = () => {
 
             const data = response.data;
             setLike(data);
-
+            console.log("add favorite!!!!");
+            console.log("data:", data);
+            console.log("favorite:", favorite);
 
         }
         catch (error) {
